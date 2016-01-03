6f44288acb3c1d78868d6434ddeb48aa2445cdfd
Juro Bystricky <jurobystricky@hotmail.com>
2016-01-03 11:33:21 -0800

    vht.h: Updated function prototypes
    Updated file to reflect the actual state.
    Several routines are removed (became static),
    some became BOOLEAN, etc.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/vht.h
+++ include/vht.h
@@ -21,7 +21,7 @@
 
 	Revision History:
 	Who 		When			What
-	--------	----------		----------------------------------------------
+	--------	----------		--------------------------------
 */
 
 #include "dot11ac_vht.h"
@@ -32,7 +32,6 @@ struct _RT_PHY_INFO;
 
 
 VOID dump_vht_cap(struct _RTMP_ADAPTER *pAd, VHT_CAP_IE *vht_ie);
-VOID dump_vht_op(struct _RTMP_ADAPTER *pAd, VHT_OP_IE *vht_ie);
 
 INT build_vht_txpwr_envelope(struct _RTMP_ADAPTER *pAd, UCHAR *buf);
 INT build_vht_ies(struct _RTMP_ADAPTER *pAd, UCHAR *buf, UCHAR frm);
@@ -40,19 +39,18 @@ INT build_vht_cap_ie(struct _RTMP_ADAPTER *pAd, UCHAR *buf);
 
 UCHAR vht_prim_ch_idx(UCHAR vht_cent_ch, UCHAR prim_ch);
 UCHAR vht_cent_ch_freq(struct _RTMP_ADAPTER *pAd, UCHAR prim_ch);
-INT vht_mode_adjust(struct _RTMP_ADAPTER *pAd, MAC_TABLE_ENTRY *pEntry, VHT_CAP_IE *cap, VHT_OP_IE *op);
-INT SetCommonVHT(struct _RTMP_ADAPTER *pAd);
+BOOLEAN vht_mode_adjust(struct _RTMP_ADAPTER *pAd, MAC_TABLE_ENTRY *pEntry, VHT_CAP_IE *cap, VHT_OP_IE *op);
+BOOLEAN SetCommonVHT(struct _RTMP_ADAPTER *pAd);
 VOID rtmp_set_vht(struct _RTMP_ADAPTER *pAd, struct _RT_PHY_INFO *phy_info);
 
 #ifdef VHT_TXBF_SUPPORT
 VOID trigger_vht_ndpa(struct _RTMP_ADAPTER *pAd, MAC_TABLE_ENTRY *entry);
-#endif /* VHT_TXBF_SUPPORT */
+#endif
 
-void assoc_vht_info_debugshow(
-	IN RTMP_ADAPTER *pAd,
-	IN MAC_TABLE_ENTRY *pEntry,
-	IN VHT_CAP_IE *vht_cap,
-	IN VHT_OP_IE *vht_op);
+#ifdef DOT11_VHT_AC
+void assoc_vht_info_debugshow(RTMP_ADAPTER *pAd, MAC_TABLE_ENTRY *pEntry,
+	VHT_CAP_IE *vht_cap, VHT_OP_IE *vht_op);
+#endif
 
 BOOLEAN vht80_channel_group( struct _RTMP_ADAPTER *pAd, UCHAR channel);
 
