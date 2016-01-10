85e49af09ff7ca827c26c119e23ffd883d7899a4
Juro Bystricky <jurobystricky@hotmail.com>
2016-01-09 16:14:31 -0800

    rtmp_usb.h: Cleanup
    Also removed prototypes of unused functions.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/iface/rtmp_usb.h
+++ include/iface/rtmp_usb.h
@@ -5,75 +5,69 @@
 
 extern UCHAR EpToQueue[6];
 
-#define RXBULKAGGRE_SIZE			12
-#define MAX_TXBULK_LIMIT			(LOCAL_TXBUF_SIZE*(BULKAGGRE_SIZE-1))
+#define RXBULKAGGRE_SIZE		12
+#define MAX_TXBULK_LIMIT		(LOCAL_TXBUF_SIZE*(BULKAGGRE_SIZE-1))
 #define MAX_TXBULK_SIZE			(LOCAL_TXBUF_SIZE*BULKAGGRE_SIZE)
 #define MAX_RXBULK_SIZE			(LOCAL_TXBUF_SIZE*RXBULKAGGRE_SIZE)
-#define MAX_MLME_HANDLER_MEMORY 20
-#define CMD_RSP_BULK_SIZE	1024
+#define MAX_MLME_HANDLER_MEMORY 	20
+#define CMD_RSP_BULK_SIZE		1024
 
 /*Power saving */
-#define PowerWakeCID		3
-#define CID0MASK		0x000000ff
-#define CID1MASK		0x0000ff00
-#define CID2MASK		0x00ff0000
-#define CID3MASK		0xff000000
-
+#define PowerWakeCID			3
+#define CID0MASK			0x000000ff
+#define CID1MASK			0x0000ff00
+#define CID2MASK			0x00ff0000
+#define CID3MASK			0xff000000
 
 /* Flags for Bulkflags control for bulk out data */
-/* */
-#define	fRTUSB_BULK_OUT_DATA_NULL			0x00000001
-#define	fRTUSB_BULK_OUT_RTS					0x00000002
-#define	fRTUSB_BULK_OUT_MLME				0x00000004
 
-#define	fRTUSB_BULK_OUT_PSPOLL				0x00000010
-#define	fRTUSB_BULK_OUT_DATA_FRAG			0x00000020
-#define	fRTUSB_BULK_OUT_DATA_FRAG_2			0x00000040
-#define	fRTUSB_BULK_OUT_DATA_FRAG_3			0x00000080
-#define	fRTUSB_BULK_OUT_DATA_FRAG_4			0x00000100
+#define	fRTUSB_BULK_OUT_DATA_NULL	0x00000001
+#define	fRTUSB_BULK_OUT_RTS		0x00000002
+#define	fRTUSB_BULK_OUT_MLME		0x00000004
 
-#define	fRTUSB_BULK_OUT_DATA_NORMAL			0x00010000
-#define	fRTUSB_BULK_OUT_DATA_NORMAL_2		0x00020000
-#define	fRTUSB_BULK_OUT_DATA_NORMAL_3		0x00040000
-#define	fRTUSB_BULK_OUT_DATA_NORMAL_4		0x00080000
+#define	fRTUSB_BULK_OUT_PSPOLL		0x00000010
+#define	fRTUSB_BULK_OUT_DATA_FRAG	0x00000020
+#define	fRTUSB_BULK_OUT_DATA_FRAG_2	0x00000040
+#define	fRTUSB_BULK_OUT_DATA_FRAG_3	0x00000080
+#define	fRTUSB_BULK_OUT_DATA_FRAG_4	0x00000100
+
+#define	fRTUSB_BULK_OUT_DATA_NORMAL	0x00010000
+#define	fRTUSB_BULK_OUT_DATA_NORMAL_2	0x00020000
+#define	fRTUSB_BULK_OUT_DATA_NORMAL_3	0x00040000
+#define	fRTUSB_BULK_OUT_DATA_NORMAL_4	0x00080000
 
 /* TODO:move to ./ate/include/iface/ate_usb.h */
 #ifdef RALINK_ATE
-#define	fRTUSB_BULK_OUT_DATA_ATE			0x00100000
-#endif /* RALINK_ATE */
+#define	fRTUSB_BULK_OUT_DATA_ATE	0x00100000
+#endif
 
 
 #define FREE_HTTX_RING(_pCookie, _pipeId, _txContext)			\
-{										\
+{									\
 	if ((_txContext)->ENextBulkOutPosition == (_txContext)->CurWritePosition)	\
-	{																	\
+	{								\
 		(_txContext)->bRingEmpty = TRUE;			\
-	}																	\
+	}								\
 	/*NdisInterlockedDecrement(&(_p)->TxCount); */\
 }
 
-#define NT_SUCCESS(status)			(((status) >=0) ? (TRUE):(FALSE))
+#define NT_SUCCESS(status)	(((status) >=0) ? (TRUE):(FALSE))
 
 
-
-
-#define PIRP		PVOID
-/*#define NDIS_OID	UINT */
+#define PIRP			PVOID
 #ifndef USB_ST_NOERROR
-#define USB_ST_NOERROR     0
+#define USB_ST_NOERROR		0
 #endif
-
 
 /* vendor-specific control operations */
 #define CONTROL_TIMEOUT_JIFFIES ( (300 * OS_HZ) / 1000)
 /*#define UNLINK_TIMEOUT_MS		3 // os abl move */
 
-
-#define DEVICE_CLASS_REQUEST_OUT		0x20
-#define DEVICE_VENDOR_REQUEST_OUT       0x40
-#define DEVICE_VENDOR_REQUEST_IN        0xc0
-/*#define INTERFACE_VENDOR_REQUEST_OUT    0x41 */
-/*#define INTERFACE_VENDOR_REQUEST_IN     0xc1 */
+#define DEVICE_CLASS_REQUEST_OUT	0x20
+#define DEVICE_VENDOR_REQUEST_OUT	0x40
+#define DEVICE_VENDOR_REQUEST_IN	0xc0
+/*#define INTERFACE_VENDOR_REQUEST_OUT	0x41 */
+/*#define INTERFACE_VENDOR_REQUEST_IN	0xc1 */
 #define BULKOUT_MGMT_RESET_FLAG		0x80
 
 #define RTUSB_SET_BULK_FLAG(_M, _F)	((_M)->BulkFlags |= (_F))
@@ -83,15 +77,9 @@ extern UCHAR EpToQueue[6];
 struct _MGMT_STRUC;
 struct _TX_BLK;
 
-NTSTATUS RTUSB_VendorRequest(
-	IN	struct _RTMP_ADAPTER *pAd,
-	IN	UINT32 TxFlags,
-	IN	UCHAR ReservedBits,
-	IN	UCHAR Request,
-	IN	USHORT val,
-	IN	USHORT idx,
-	IN	PVOID txbuf,
-	IN	UINT32 txbuf_len);
+NTSTATUS RTUSB_VendorRequest(struct _RTMP_ADAPTER *pAd, UINT32 TxFlags,
+	UCHAR ReservedBits, UCHAR Request, USHORT val, USHORT idx, PVOID txbuf,
+	UINT32 txbuf_len);
 
 NTSTATUS RTUSBMultiRead(struct _RTMP_ADAPTER *pAd, USHORT Offset, UCHAR *buf, USHORT len);
 NTSTATUS RTUSBMultiWrite(struct _RTMP_ADAPTER *pAd, USHORT Offset, UCHAR *buf, USHORT len, BOOLEAN bWriteHigh);
@@ -107,32 +95,20 @@ NTSTATUS RTUSBReadMACRegister(struct _RTMP_ADAPTER *pAd, USHORT Offset, UINT32 *
 
 NTSTATUS RTUSBReadEEPROM(struct _RTMP_ADAPTER *pAd, USHORT Offset, UCHAR *buf, USHORT len);
 NTSTATUS RTUSBWriteEEPROM(struct _RTMP_ADAPTER *pAd, USHORT Offset, UCHAR *buf, USHORT len);
-
 NTSTATUS RTUSBFirmwareWrite(struct _RTMP_ADAPTER *pAd, UCHAR *pFwImage, ULONG FwLen);
-
 NTSTATUS RTUSBVenderReset(struct _RTMP_ADAPTER *pAd);
-
 BOOLEAN AsicCheckCommandOk(struct _RTMP_ADAPTER *pAd, UCHAR cmd);
 
-
-NDIS_STATUS	RTUSBEnqueueCmdFromNdis(struct _RTMP_ADAPTER *pAd, NDIS_OID Oid, BOOLEAN SetInfo, PVOID pInfoBuf, UINT32 BufLen);
+NDIS_STATUS RTUSBEnqueueCmdFromNdis(struct _RTMP_ADAPTER *pAd, NDIS_OID Oid,
+	BOOLEAN SetInfo, PVOID pInfoBuf, UINT32 BufLen);
 VOID RTUSBDequeueCmd(PCmdQ cmdq, PCmdQElmt *pcmdqelmt);
-INT RTUSBCmdThread(ULONG Context);
-
 
 VOID RTUSBBssBeaconExit(struct _RTMP_ADAPTER *pAd);
 VOID RTUSBBssBeaconStop(struct _RTMP_ADAPTER *pAd);
 VOID RTUSBBssBeaconStart(struct _RTMP_ADAPTER * pAd);
 VOID RTUSBBssBeaconInit(struct _RTMP_ADAPTER *pAd);
 
-
-NDIS_STATUS RTUSBSetHardWareRegister(struct _RTMP_ADAPTER *pAd, PVOID pBuf);
-NDIS_STATUS	RTUSBWriteHWMACAddress(struct _RTMP_ADAPTER *pAd);
-NDIS_STATUS RTUSBQueryHardWareRegister(struct _RTMP_ADAPTER *pAd, PVOID pBuf);
-
-VOID RTUSBMlmeHardTransmit(struct _RTMP_ADAPTER *pAd, struct _MGMT_STRUC *pMgmt);
-
-NDIS_STATUS	RTUSBFreeDescRequest(struct _RTMP_ADAPTER *pAd, UCHAR BulkOutPipeId, UINT32 req_cnt);
+NDIS_STATUS RTUSBFreeDescRequest(struct _RTMP_ADAPTER *pAd, UCHAR BulkOutPipeId, UINT32 req_cnt);
 BOOLEAN	RTUSBNeedQueueBackForAgg(struct _RTMP_ADAPTER *pAd, UCHAR BulkOutPipeId);
 
 USHORT RtmpUSB_WriteSubTxResource(struct _RTMP_ADAPTER *pAd, struct _TX_BLK *pTxBlk, BOOLEAN bIsLast, USHORT *freeCnt);
@@ -150,20 +126,16 @@ VOID RTUSBWatchDog(struct _RTMP_ADAPTER *pAd);
 
 VOID RTUSBPutToSleep(struct _RTMP_ADAPTER *pAd);
 NTSTATUS RTUSBWakeUp(struct _RTMP_ADAPTER *pAd);
-	
+
 VOID RtmpUsbStaAsicForceWakeupTimeout(PVOID arg1, PVOID FuncContext, PVOID arg2, PVOID arg3);
 
 VOID RT28xxUsbStaAsicForceWakeup(struct _RTMP_ADAPTER *pAd, BOOLEAN bFromTx);
-
 VOID RT28xxUsbStaAsicSleepThenAutoWakeup(struct _RTMP_ADAPTER *pAd, USHORT TbttNumToNextWakeUp);
 
 VOID RT28xxUsbMlmeRadioOn(struct _RTMP_ADAPTER *pAd);
 VOID RT28xxUsbMlmeRadioOFF(struct _RTMP_ADAPTER *pAd);
 VOID RT28xxUsbAsicRadioOn(struct _RTMP_ADAPTER *pAd);
 VOID RT28xxUsbAsicRadioOff(struct _RTMP_ADAPTER *pAd);
-
-VOID RT28xxUsbAsicWOWEnable(struct _RTMP_ADAPTER *pAd);
-VOID RT28xxUsbAsicWOWDisable(struct _RTMP_ADAPTER *pAd);
 
 struct usb_control {
 	BOOLEAN usb_aggregation;
