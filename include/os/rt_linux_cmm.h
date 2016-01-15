54224abbfa2c07bec2f266be7ce315c955853073
Juro Bystricky <jurobystricky@hotmail.com>
2016-01-14 20:54:36 -0800

    rt_linux_cmm.h: Removed unused definitions
    Removed tons of unneeded definitions.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/os/rt_linux_cmm.h
+++ include/os/rt_linux_cmm.h
@@ -34,47 +34,11 @@ typedef struct _OS_RSTRUC  {
 } OS_RSTRUC;
 
 
-/* declare new chipset function here */
-#ifdef OS_ABL_FUNC_SUPPORT
-
-#define RTMP_DECLARE_DRV_OPS_FUNCTION(_func)				\
-	void Rtmp_Drv_Ops_##_func(VOID *__pDrvOps, VOID *__pNetOps, 	\
-			VOID *__pPciConfig, VOID *__pUsbConfig)
-
-#define RTMP_BUILD_DRV_OPS_FUNCTION(_func)				\
-	void Rtmp_Drv_Ops_##_func(VOID *__pDrvOps, VOID *__pNetOps, 	\
-	void *__pPciConfig, VOID *__pUsbConfig)				\
-{									\
-	RtmpDrvOpsInit(__pDrvOps, __pNetOps, __pPciConfig, __pUsbConfig);\
-}
-
-#define RTMP_GET_DRV_OPS_FUNCTION(_func)				\
-	(PVOID)Rtmp_Drv_Ops_##_func
-
-#define RTMP_DRV_OPS_FUNCTION_BODY(_func)				\
-	Rtmp_Drv_Ops_##_func
-
-
-#define xdef_to_str(s)   def_to_str(s)
-#define def_to_str(s)    #s
-
-
-#ifdef RTMP_MAC_USB
-#define RTMP_DRV_NAME	"rtusb" xdef_to_str(RT28xx_MODE)
-RTMP_DECLARE_DRV_OPS_FUNCTION(usb);
-#define RTMP_DRV_OPS_FUNCTION			RTMP_DRV_OPS_FUNCTION_BODY(usb)
-#define RTMP_BUILD_DRV_OPS_FUNCTION_BODY	RTMP_BUILD_DRV_OPS_FUNCTION(usb)
-#endif /* RTMP_MAC_USB */
-
-#else
-
 #ifdef RTMP_MAC_USB
 #define RTMP_DRV_NAME	"rt2870"
 #else
 #define RTMP_DRV_NAME	"rt2860"
 #endif /* RTMP_MAC_USB */
-
-#endif /* OS_ABL_FUNC_SUPPORT */
 
 
 /*****************************************************************************
@@ -82,8 +46,6 @@ RTMP_DECLARE_DRV_OPS_FUNCTION(usb);
  ******************************************************************************/
 #define RTMP_OS_TASK_INIT(__pTask, __pTaskName, __pAd)		\
 	RtmpOSTaskInit(__pTask, __pTaskName, __pAd, &(__pAd)->RscTaskMemList, &(__pAd)->RscSemMemList);
-
-#ifndef OS_ABL_FUNC_SUPPORT
 
 /* rt_linux.h */
 #define RTMP_OS_TASK				OS_TASK
@@ -106,28 +68,10 @@ RTMP_DECLARE_DRV_OPS_FUNCTION(usb);
 #define RTMP_OS_TASK_LEGALITY(__pTask)		CHECK_PID_LEGALITY((__pTask)->taskPID)
 #endif /* KTHREAD_SUPPORT */
 
-#else
-
-/* rt_linux_cmm.h */
-#define RTMP_OS_TASK				OS_RSTRUC
-
-#define RTMP_OS_TASK_GET(__pTask)		((OS_TASK *)((__pTask)->pContent))
-
-#define RTMP_OS_TASK_DATA_GET(__pTask)		RtmpOsTaskDataGet(__pTask)
-
-#define RTMP_OS_TASK_IS_KILLED(__pTask)		RtmpOsTaskIsKilled(__pTask)
-
-#define RTMP_OS_TASK_WAKE_UP(__pTask)		RtmpOsTaskWakeUp(pTask)
-
-#define RTMP_OS_TASK_LEGALITY(__pTask)		if (RtmpOsCheckTaskLegality(__pTask))
-
-#endif /* OS_ABL_FUNC_SUPPORT */
-
 
 /*****************************************************************************
  * Timer related definitions and data structures.
  ******************************************************************************/
-#ifndef OS_ABL_FUNC_SUPPORT
 
 /* rt_linux.h */
 #define NDIS_MINIPORT_TIMER			OS_NDIS_MINIPORT_TIMER
@@ -140,21 +84,6 @@ RTMP_DECLARE_DRV_OPS_FUNCTION(usb);
 #define RTMP_OS_FREE_SEM(__pAd)
 #define RTMP_OS_FREE_ATOMIC(__pAd)
 
-#else
-
-/* rt_linux_cmm.h */
-#define NDIS_MINIPORT_TIMER			OS_RSTRUC
-#define RTMP_OS_TIMER				OS_RSTRUC
-
-#define RTMP_OS_FREE_TIMER(__pAd)
-#define RTMP_OS_FREE_LOCK(__pAd)
-#define RTMP_OS_FREE_TASKLET(__pAd)
-#define RTMP_OS_FREE_TASK(__pAd)
-#define RTMP_OS_FREE_SEM(__pAd)
-#define RTMP_OS_FREE_ATOMIC(__pAd)
-
-#endif /* OS_ABL_FUNC_SUPPORT */
-
 
 /*****************************************************************************
  *	OS file operation related data structure definitions
@@ -164,25 +93,12 @@ RTMP_DECLARE_DRV_OPS_FUNCTION(usb);
 #define RTMP_FILE_WRONLY			0x0F02
 #define RTMP_FILE_CREAT				0x0F03
 #define RTMP_FILE_TRUNC				0x0F04
-
-#ifndef OS_ABL_FUNC_SUPPORT
-
-/* rt_linux.h */
 #define RTMP_OS_FS_INFO				OS_FS_INFO
-
-#else
-
-/* rt_linux_cmm.h */
-#define RTMP_OS_FS_INFO				OS_RSTRUC
-
-#endif /* OS_ABL_FUNC_SUPPORT */
 
 
 /*****************************************************************************
  *	OS semaphore related data structure and definitions
  ******************************************************************************/
-
-#ifndef OS_ABL_FUNC_SUPPORT
 
 #define NDIS_SPIN_LOCK			OS_NDIS_SPIN_LOCK
 #define NdisAllocateSpinLock(__pReserved, __pLock)	OS_NdisAllocateSpinLock(__pLock)
@@ -217,84 +133,11 @@ RTMP_DECLARE_DRV_OPS_FUNCTION(usb);
 #define RTMP_OS_ATMOIC_DESTROY(__pAtomic)
 #define RTMP_THREAD_PID_KILL(__PID)				KILL_THREAD_PID(__PID, SIGTERM, 1)
 
-#else
-#error HERE???
-#define NDIS_SPIN_LOCK					OS_RSTRUC
-#define RTMP_OS_SEM					OS_RSTRUC
-#define RTMP_OS_ATOMIC					OS_RSTRUC
-
-#define RTMP_SEM_EVENT_INIT_LOCKED 			RtmpOsSemaInitLocked
-#define RTMP_SEM_EVENT_INIT				RtmpOsSemaInit
-#define RTMP_SEM_EVENT_DESTORY				RtmpOsSemaDestory
-#define RTMP_SEM_EVENT_WAIT(_pSema, _status)	((_status) = RtmpOsSemaWaitInterruptible((_pSema)))
-#define RTMP_SEM_EVENT_UP				RtmpOsSemaWakeUp
-
-#define RtmpMLMEUp					RtmpOsMlmeUp
-
-#define RTMP_OS_ATMOIC_INIT				RtmpOsAtomicInit
-#define RTMP_OS_ATMOIC_DESTROY				RtmpOsAtomicDestroy
-#define RTMP_THREAD_PID_KILL				RtmpThreadPidKill
-
-/* */
-/*  spin_lock enhanced for Nested spin lock */
-/* */
-#define NdisAllocateSpinLock(__pAd, __pLock)		RtmpOsAllocateLock(__pLock, &(__pAd)->RscLockMemList)
-#define NdisFreeSpinLock				RtmpOsFreeSpinLock
-
-#define RTMP_SEM_LOCK(__lock)						\
-{									\
-	RtmpOsSpinLockBh(__lock);					\
-}
-
-#define RTMP_SEM_UNLOCK(__lock)						\
-{									\
-	RtmpOsSpinUnLockBh(__lock);					\
-}
-#define RTMP_SPIN_LOCK_IRQ				RtmpOsSpinLockIrq
-#define RTMP_SPIN_UNLOCK_IRQ				RtmpOsSpinUnlockIrq
-#define RTMP_SPIN_LOCK_IRQSAVE				RtmpOsSpinLockIrqSave
-#define RTMP_SPIN_UNLOCK_IRQRESTORE			RtmpOsSpinUnlockIrqRestore
-
-/* sample, use semaphore lock to replace IRQ lock, 2007/11/15 */
-#ifdef MULTI_CORE_SUPPORT
-
-#define RTMP_IRQ_LOCK(__lock, __irqflags)				\
-{									\
-	spin_lock_irqsave((__lock), __irqflags);			\
-}
-
-#define RTMP_IRQ_UNLOCK(__lock, __irqflag)				\
-{									\
-	spin_unlock_irqrestore((__lock), __irqflag);	\
-}
-#else
-#define RTMP_IRQ_LOCK(__lock, __irqflags)				\
-{									\
-	__irqflags = 0;							\
-	RtmpOsSpinLockBh(__lock);					\
-}
-
-#define RTMP_IRQ_UNLOCK(__lock, __irqflag)				\
-{									\
-	RtmpOsSpinUnLockBh(__lock);					\
-}
-#endif // MULTI_CORE_SUPPORT //
-#define RTMP_INT_LOCK(__Lock, __Flag)	RtmpOsIntLock(__Lock, &__Flag)
-#define RTMP_INT_UNLOCK					RtmpOsIntUnLock
-
-#define NdisAcquireSpinLock				RTMP_SEM_LOCK
-#define NdisReleaseSpinLock				RTMP_SEM_UNLOCK
-
-#endif /* OS_ABL_FUNC_SUPPORT */
-
 
 /*****************************************************************************
  *	OS task related data structure and definitions
  ******************************************************************************/
 
-#ifndef OS_ABL_FUNC_SUPPORT
-
-/* rt_linux.h */
 #define RTMP_NET_TASK_STRUCT		OS_NET_TASK_STRUCT
 #define PRTMP_NET_TASK_STRUCT		POS_NET_TASK_STRUCT
 
@@ -330,66 +173,23 @@ typedef struct completion RTMP_OS_COMPLETION;
 #define RTMP_NET_TASK_DATA_ASSIGN(__Tasklet, __Data)			\
 	(__Tasklet)->data = (unsigned long)__Data
 
-#else
-
-/* rt_linux_cmm.h */
-typedef OS_RSTRUC RTMP_NET_TASK_STRUCT;
-typedef OS_RSTRUC *PRTMP_NET_TASK_STRUCT;
-typedef OS_RSTRUC RTMP_OS_COMPLETION;
-typedef OS_RSTRUC *PRTMP_OS_COMPLETION;
-
-#define RTMP_OS_INIT_COMPLETION(__pCompletion)	\
-		RtmpOsInitCompletion(__pCompletion)
-
-#define RTMP_OS_EXIT_COMPLETION(__pCompletion)	\
-		RtmpOsExitCompletion(__pCompletion)
-
-#define RTMP_OS_COMPLETE(__pCompletion)	\
-		RtmpOsComplete(__pCompletion)
-
-#define RTMP_OS_WAIT_FOR_COMPLETION_TIMEOUT(__pCompletion, __Timeout)	\
-		RtmpOsWaitForCompletionTimeout(__pCompletion, __Timeout)
-
-#define RTMP_OS_TASKLET_SCHE(__pTasklet)				\
-		RtmpOsTaskletSche(__pTasklet)
-
-#define RTMP_OS_TASKLET_INIT(__pAd, __pTasklet, __pFunc, __Data)	\
-		RtmpOsTaskletInit(__pTasklet, __pFunc, __Data, &(__pAd)->RscTaskletMemList)
-
-#define RTMP_OS_TASKLET_KILL(__pTasklet)				\
-		RtmpOsTaskletKill(__pTasklet)
-
-#define RTMP_NET_TASK_DATA_ASSIGN(__pTasklet, __Data)			\
-		RtmpOsTaskletDataAssign(__pTasklet, __Data)
-
-#endif /* OS_ABL_FUNC_SUPPORT */
-
-
 
 /*****************************************************************************
  *	OS definition related data structure and definitions
  ******************************************************************************/
-
 
 #ifdef RTMP_USB_SUPPORT
 #define RTMP_USB_CONTROL_MSG_ENODEV	(-ENODEV)
 #define RTMP_USB_CONTROL_MSG_FAIL	(-EFAULT)
 #endif /* RTMP_USB_SUPPORT */
 
-#define RTMP_OS_PCI_VENDOR_ID		PCI_VENDOR_ID
-#define RTMP_OS_PCI_DEVICE_ID		PCI_DEVICE_ID
-
 #define ra_dma_addr_t			dma_addr_t
-
-
-#define PCI_MAP_SINGLE			RtmpDrvPciMapSingle
 
 
 /***********************************************************************************
  *	Others
  ***********************************************************************************/
 #define APCLI_IF_UP_CHECK(pAd, ifidx) (RtmpOSNetDevIsUp((pAd)->ApCfg.ApCliTab[(ifidx)].wdev.if_dev) == TRUE)
-
 
 #define RTMP_OS_NETDEV_SET_PRIV		RtmpOsSetNetDevPriv
 #define RTMP_OS_NETDEV_GET_PRIV		RtmpOsGetNetDevPriv
