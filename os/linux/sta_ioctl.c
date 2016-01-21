daccfa2b07f615bc91b8532a189f4a477f5bb6d4
Juro Bystricky <jurobystricky@hotmail.com>
2016-01-21 05:39:46 -0800

    sta_ioctl.c: Fix broken build if CONFIG_WEXT_PRIV not set
    iw_handler_def has some fields only defined if the
    driver was configured with CONFIG_WEXT_PRIV.
    Some other minor changes.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- os/linux/sta_ioctl.c
+++ os/linux/sta_ioctl.c
@@ -2068,10 +2068,12 @@ const struct iw_handler_def rt28xx_iw_handler_def =
 #define	N(a)	(sizeof (a) / sizeof (a[0]))
 	.standard		= (iw_handler *) rt_handler,
 	.num_standard		= sizeof(rt_handler) / sizeof(iw_handler),
+#ifdef CONFIG_WEXT_PRIV
 	.private		= (iw_handler *) rt_priv_handlers,
 	.num_private		= N(rt_priv_handlers),
 	.private_args		= (struct iw_priv_args *) privtab,
 	.num_private_args	= N(privtab),
+#endif
 #if IW_HANDLER_VERSION >= 7
 	.get_wireless_stats 	= rt28xx_get_wireless_stats,
 #endif
