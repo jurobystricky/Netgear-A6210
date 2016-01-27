f5ce2542f9c0b87792d8ded071975e5164b40ebd
Juro Bystricky <jurobystricky@hotmail.com>
2016-01-26 21:08:17 -0800

    mt76x2.h: Various changes
    Modified some prototypes as BOOLEAN.
    Removed uneeded/unused prototypes.
    Basic cleanup.
    Removed some unneeded definitions.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/chip/mt76x2.h
+++ include/chip/mt76x2.h
@@ -4,53 +4,38 @@
 #include "../mcu/mcu_and.h"
 #include "../phy/mt_rf.h"
 
-struct _RTMP_ADAPTER;
-
 #define MAX_RF_ID	127
-#define MAC_RF_BANK 7
 
 void mt76x2_init(struct _RTMP_ADAPTER *ad);
 void mt76x2_adjust_per_rate_pwr_delta(struct _RTMP_ADAPTER *ad, u8 channel, char delta_pwr);
 void mt76x2_get_tx_pwr_per_rate(struct _RTMP_ADAPTER *ad);
-int mt76x2_read_chl_pwr(struct _RTMP_ADAPTER *ad);
+BOOLEAN mt76x2_read_chl_pwr(struct _RTMP_ADAPTER *ad);
 void mt76x2_pwrOn(struct _RTMP_ADAPTER *ad);
 void mt76x2_calibration(struct _RTMP_ADAPTER *ad, u8 channel);
-void mt76x2_external_pa_rf_dac_control(struct _RTMP_ADAPTER *ad, u8 channel);
 void mt76x2_tssi_calibration(struct _RTMP_ADAPTER *ad, u8 channel);
 void mt76x2_tssi_compensation(struct _RTMP_ADAPTER *ad, u8 channel);
-int mt76x2_set_ed_cca(struct _RTMP_ADAPTER *ad, u8 enable);
+BOOLEAN mt76x2_set_ed_cca(struct _RTMP_ADAPTER *ad, u8 enable);
 int mt76x2_reinit_agc_gain(struct _RTMP_ADAPTER *ad, u8 channel);
 int mt76x2_reinit_hi_lna_gain(struct _RTMP_ADAPTER *ad, u8 channel);
 void mt76x2_get_external_lna_gain(struct _RTMP_ADAPTER *ad);
 void mt76x2_get_agc_gain(struct _RTMP_ADAPTER *ad, BOOLEAN init_phase);
-//jb was char get_chl_grp(u8 channel);
 int get_chl_grp(u8 channel);
-void percentage_delta_pwr(struct _RTMP_ADAPTER *ad);
-
 void mt76x2_get_current_temp(struct _RTMP_ADAPTER *ad);
 void mt76x2_read_temp_info_from_eeprom(struct _RTMP_ADAPTER *ad);
+
 #ifdef RTMP_TEMPERATURE_TX_ALC
 void mt76x2_read_tx_alc_info_from_eeprom(struct _RTMP_ADAPTER *ad);
-void mt76x2_temp_tx_alc(struct _RTMP_ADAPTER *ad);
-#endif /* RTMP_TEMPERATURE_TX_ALC */
+#endif
 
 #ifdef SINGLE_SKU_V2
 void mt76x2_single_sku(struct _RTMP_ADAPTER *ad, u8 channel);
 void mt76x2_read_single_sku_info_from_eeprom(struct _RTMP_ADAPTER *ad);
-void mt76x2_make_up_rate_pwr_table(struct _RTMP_ADAPTER *ad);
-UCHAR mt76x2_get_sku_channel_base_pwr(struct _RTMP_ADAPTER *ad, u8 channel);
-void mt76x2_update_per_rate_pwr(struct _RTMP_ADAPTER *ad);
-UCHAR mt76x2_update_sku_pwr(struct _RTMP_ADAPTER *ad, u8 channel);
-#endif /* SINGLE_SKU_V2 */
-
-#ifdef ED_MONITOR
-void mt7612_set_ed_cca(struct _RTMP_ADAPTER *ad, BOOLEAN enable);
-#endif /* ED_MONITOR */
+#endif
 
 #ifdef RALINK_ATE
-VOID mt76x2_ate_do_calibration(
+void mt76x2_ate_do_calibration(
 	struct _RTMP_ADAPTER *ad, UINT32 cal_id, UINT32 param);
-#endif /* RALINK_ATE */
+#endif
 
 struct mt76x2_frequency_item {
 	u8 channel;
