18a8009612e7f9ff7f3373f680d05c3c087c6104
Juro Bystricky <jurobystricky@hotmail.com>
2016-01-26 20:53:34 -0800

    rt_linux_cmm.h: Removed NDIS_SPIN_LOCK definition
    NDIS_SPIN_LOCK was definead as OS_NDIS_SPIN_LOCK, which in turn
    was defined as spinock_t. So all occurences of NDIS_SPIN_LOCK
    and OS_NDIS_SPINLOCK were replaced by spinlock_t, making
    NDIS_SPIN_LOCK and OS_NDIS_SPIN_LOCK unneeded and code more
    obvious.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/os/rt_linux_cmm.h
+++ include/os/rt_linux_cmm.h
@@ -99,7 +99,6 @@ typedef struct _OS_RSTRUC  {
  *	OS semaphore related data structure and definitions
  ******************************************************************************/
 
-#define NDIS_SPIN_LOCK			OS_NDIS_SPIN_LOCK
 #define NdisAllocateSpinLock(__pReserved, __pLock)	OS_NdisAllocateSpinLock(__pLock)
 #define NdisFreeSpinLock		OS_NdisFreeSpinLock
 #define RTMP_SEM_LOCK			OS_SEM_LOCK
