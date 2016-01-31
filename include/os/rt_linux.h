1b56feb24b6fe85d30679cfd82d22bf2033ca398
Juro Bystricky <jurobystricky@hotmail.com>
2016-01-30 20:00:25 -0800

    rt_linux.h: Modified MlmeAllocateMemory, MlmeFreeMemory
    These macros needed to be changed, as the prototypes for
    os_alloc_mem, os_free_mem have changed.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/os/rt_linux.h
+++ include/os/rt_linux.h
@@ -382,7 +382,7 @@ do { \
 #define OS_SEM_EVENT_WAIT(_pSema, _status)	((_status) = down_interruptible((_pSema)))
 #define OS_SEM_EVENT_UP(_pSema)			up(_pSema)
 
-#define RTCMDUp					OS_RTCMDUp
+//#define RTCMDUp					OS_RTCMDUp
 
 #ifdef KTHREAD_SUPPORT
 #define RTMP_WAIT_EVENT_INTERRUPTIBLE(_Status, _pTask) \
@@ -423,8 +423,8 @@ do { \
 #define NdisEqualMemory(Source1, Source2, Length)   (!memcmp(Source1, Source2, Length))
 #define RTMPEqualMemory(Source1, Source2, Length)   (!memcmp(Source1, Source2, Length))
 
-#define MlmeAllocateMemory(_pAd, _ppVA)		os_alloc_mem(_pAd, _ppVA, MGMT_DMA_BUFFER_SIZE)
-#define MlmeFreeMemory(_pAd, _pVA)		os_free_mem(_pAd, _pVA)
+#define MlmeAllocateMemory()		os_alloc_mem(MGMT_DMA_BUFFER_SIZE)
+#define MlmeFreeMemory(_pVA)		os_free_mem(_pVA)
 
 #define COPY_MAC_ADDR(Addr1, Addr2)		memcpy((Addr1), (Addr2), MAC_ADDR_LEN)
 
