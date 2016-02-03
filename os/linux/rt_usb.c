7dda120b4d17aaa421072e6d2c2aa476014f9e24
Juro Bystricky <jurobystricky@hotmail.com>
2016-02-03 07:33:11 -0800

    rt_usb.c: De-obfuscated RTMPFreeNdisPacket
    Replaced the two argument aberration with the actual
    code.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- os/linux/rt_usb.c
+++ os/linux/rt_usb.c
@@ -487,7 +487,7 @@ static void rtusb_mgmt_dma_done_tasklet(unsigned long data)
 
 	/* No-matter success or fail, we free the mgmt packet. */
 	if (pPacket)
-		RTMPFreeNdisPacket(pAd, pPacket);
+		dev_kfree_skb_any(pPacket);
 
 	if ((RTMP_TEST_FLAG(pAd, (fRTMP_ADAPTER_RESET_IN_PROGRESS |
 		fRTMP_ADAPTER_HALT_IN_PROGRESS | fRTMP_ADAPTER_NIC_NOT_EXIST)))) {
