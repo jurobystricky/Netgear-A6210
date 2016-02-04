943efe3bf9cfc057d095b31f3098e679f141a157
Juro Bystricky <jurobystricky@hotmail.com>
2016-02-03 22:13:59 -0800

    sta.h: Cleanup
    Use C types for INT and VOID.
    Remove unused prototypes.
    Cosmetic changes.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/sta.h
+++ include/sta.h
@@ -21,80 +21,42 @@
     Abstract:
     Miniport generic portion header file
 
-    Revision History:
-    Who         When          What
-    --------    ----------    ----------------------------------------------
 */
 
 #ifndef __STA_H__
 #define __STA_H__
 
+BOOLEAN RTMPCheckChannel(PRTMP_ADAPTER pAd, UCHAR CentralChannel, UCHAR Channel);
 
-BOOLEAN RTMPCheckChannel(
-	IN PRTMP_ADAPTER pAd,
-	IN UCHAR		CentralChannel,
-	IN UCHAR		Channel);
+void AdjustChannelRelatedValue(PRTMP_ADAPTER pAd, UCHAR *pBwFallBack,
+	USHORT ifIndex, BOOLEAN BandWidth, UCHAR PriCh, UCHAR ExtraCh);
 
-//VOID InitChannelRelatedValue(
-//	IN  PRTMP_ADAPTER   pAd);
+void RTMPReportMicError(PRTMP_ADAPTER pAd, PCIPHER_KEY pWpaKey);
 
-VOID AdjustChannelRelatedValue(
-	IN PRTMP_ADAPTER pAd,
-	OUT UCHAR *pBwFallBack,
-	IN USHORT ifIndex,
-	IN BOOLEAN BandWidth,
-	IN UCHAR PriCh,
-	IN UCHAR ExtraCh);
+int RTMPCheckRxError(RTMP_ADAPTER *pAd, HEADER_802_11 *pHeader, RX_BLK *pRxBlk,
+	RXINFO_STRUC *pRxInfo);
 
-VOID RTMPReportMicError(
-	IN  PRTMP_ADAPTER   pAd, 
-	IN  PCIPHER_KEY     pWpaKey);
+void WpaMicFailureReportFrame(PRTMP_ADAPTER pAd, MLME_QUEUE_ELEM *Elem);
 
-INT RTMPCheckRxError(
-	IN RTMP_ADAPTER *pAd, 
-	IN HEADER_802_11 *pHeader,
-	IN RX_BLK *pRxBlk,
-	IN RXINFO_STRUC *pRxInfo);
+void WpaDisassocApAndBlockAssoc(PVOID SystemSpecific1, PVOID FunctionContext,
+	PVOID SystemSpecific2, PVOID SystemSpecific3);
 
-VOID WpaMicFailureReportFrame(
-	IN  PRTMP_ADAPTER    pAd, 
-	IN  MLME_QUEUE_ELEM *Elem);
+void WpaStaPairwiseKeySetting(PRTMP_ADAPTER pAd);
 
-VOID WpaDisassocApAndBlockAssoc(
-    IN  PVOID SystemSpecific1, 
-    IN  PVOID FunctionContext, 
-    IN  PVOID SystemSpecific2, 
-    IN  PVOID SystemSpecific3);
+void WpaStaGroupKeySetting(PRTMP_ADAPTER pAd);
 
-VOID WpaStaPairwiseKeySetting(
-	IN	PRTMP_ADAPTER	pAd);
+void WpaSendEapolStart(PRTMP_ADAPTER pAdapter, PUCHAR pBssid);
 
-VOID WpaStaGroupKeySetting(
-	IN	PRTMP_ADAPTER	pAd);
+void STAHandleRxDataFrame(RTMP_ADAPTER *pAd, RX_BLK *pRxBlk);
 
-VOID WpaSendEapolStart(
-	IN	PRTMP_ADAPTER	pAdapter,
-	IN  PUCHAR          pBssid);
+void STARxEAPOLFrameIndicate(PRTMP_ADAPTER pAd, MAC_TABLE_ENTRY *pEntry,
+	RX_BLK *pRxBlk, UCHAR FromWhichBSSID);
 
+NDIS_STATUS STAHardTransmit(PRTMP_ADAPTER pAd, TX_BLK *pTxBlk, UCHAR QueIdx);
 
-VOID STAHandleRxDataFrame(RTMP_ADAPTER *pAd, RX_BLK *pRxBlk);
+NDIS_STATUS STASendPacket(RTMP_ADAPTER *pAd, PNDIS_PACKET pPacket);
 
-VOID STARxEAPOLFrameIndicate(
-	IN	PRTMP_ADAPTER	pAd,
-	IN	MAC_TABLE_ENTRY	*pEntry,
-	IN	RX_BLK			*pRxBlk,
-	IN	UCHAR			FromWhichBSSID);
-
-NDIS_STATUS	STAHardTransmit(
-	IN PRTMP_ADAPTER	pAd,
-	IN TX_BLK			*pTxBlk,
-	IN  UCHAR			QueIdx);
-
-INT STASendPacket(
-	IN RTMP_ADAPTER *pAd,
-	IN PNDIS_PACKET pPacket);
-
-INT STAInitialize(RTMP_ADAPTER *pAd);
+int STAInitialize(RTMP_ADAPTER *pAd);
 
 #endif /* __STA_H__ */
 
