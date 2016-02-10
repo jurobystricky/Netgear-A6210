7892b5baad13b747a53b2c404d58c20ad0861540
Juro Bystricky <jurobystricky@hotmail.com>
2016-02-10 06:51:00 -0800

    rt_rf.c: Various changes
    Cleanup 101.
    Use C types for VOID and INT.
    Declared as static: RTMP_ReadRF
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- phy/rt_rf.c
+++ phy/rt_rf.c
@@ -21,9 +21,6 @@
 	Abstract:
 	Ralink Wireless driver RF related functions
 
-	Revision History:
-	Who         When          What
-	--------    ----------    ----------------------------------------------
 */
 
 
@@ -33,61 +30,53 @@
 #ifdef RTMP_RF_RW_SUPPORT
 /*
 	========================================================================
-	
+
 	Routine Description: Read RF register through MAC with specified bit mask
 
 	Arguments:
-		pAd		- pointer to the adapter structure
+		pAd	- pointer to the adapter structure
 		regID	- RF register ID
 		pValue1	- (RF value & BitMask)
 		pValue2	- (RF value & (~BitMask))
 		BitMask	- bit wise mask
 
 	Return Value:
-	
+
 	Note:
-	
+
 	========================================================================
 */
-VOID RTMP_ReadRF(
-	IN	PRTMP_ADAPTER	pAd,
-	IN	UCHAR			RegID,
-	OUT	PUCHAR			pValue1,
-	OUT PUCHAR			pValue2,
-	IN	UCHAR			BitMask)
-{	
-	UCHAR RfReg = 0;									
-	RT30xxReadRFRegister(pAd, RegID, &RfReg);		
-	if (pValue1 != NULL)								
-		*pValue1 = RfReg & BitMask;			
-	if (pValue2 != NULL)								
-		*pValue2 = RfReg & (~BitMask);		
+static void RTMP_ReadRF(PRTMP_ADAPTER pAd, UCHAR RegID, PUCHAR pValue1, 
+	PUCHAR pValue2, UCHAR BitMask)
+{
+	UCHAR RfReg = 0;
+	RT30xxReadRFRegister(pAd, RegID, &RfReg);
+	if (pValue1 != NULL)
+		*pValue1 = RfReg & BitMask;
+	if (pValue2 != NULL)
+		*pValue2 = RfReg & (~BitMask);
 }
 
 /*
 	========================================================================
-	
+
 	Routine Description: Write RF register through MAC with specified bit mask
 
 	Arguments:
-		pAd		- pointer to the adapter structure
+		pAd	- pointer to the adapter structure
 		regID	- RF register ID
 		Value	- only write the part of (Value & BitMask) to RF register
 		BitMask	- bit wise mask
 
 	Return Value:
-	
+
 	Note:
-	
+
 	========================================================================
 */
-VOID RTMP_WriteRF(
-	IN	PRTMP_ADAPTER	pAd,
-	IN	UCHAR			RegID,
-	IN	UCHAR			Value,
-	IN	UCHAR			BitMask)
+void RTMP_WriteRF(PRTMP_ADAPTER pAd, UCHAR RegID, UCHAR Value, UCHAR BitMask)
 {
-	UCHAR RfReg = 0;	
+	UCHAR RfReg = 0;
 	RTMP_ReadRF(pAd, RegID, NULL, &RfReg, BitMask);
 	RfReg |= ((Value) & BitMask);
 	RT30xxWriteRFRegister(pAd, RegID, RfReg);
@@ -95,28 +84,16 @@ VOID RTMP_WriteRF(
 
 /*
 	========================================================================
-	
+
 	Routine Description: Write RF register through MAC
 
-	Arguments:
-
-	Return Value:
-
-	IRQL = 
-	
-	Note:
-	
 	========================================================================
 */
-NDIS_STATUS RT30xxWriteRFRegister(
-	IN	PRTMP_ADAPTER	pAd,
-	IN	UCHAR			regID,
-	IN	UCHAR			value)
+NDIS_STATUS RT30xxWriteRFRegister(PRTMP_ADAPTER pAd, UCHAR regID, UCHAR value)
 {
 	RF_CSR_CFG_STRUC rfcsr = { { 0 } };
 	UINT i = 0;
-	NDIS_STATUS	 ret;
-
+	NDIS_STATUS ret;
 
 	ASSERT((regID <= pAd->chipCap.MaxNumOfRfId));
 
@@ -124,27 +101,29 @@ NDIS_STATUS RT30xxWriteRFRegister(
 	if (IS_USB_INF(pAd)) {
 		RTMP_SEM_EVENT_WAIT(&pAd->reg_atomic, ret);
 		if (ret != 0) {
-			DBGPRINT(RT_DEBUG_ERROR, ("reg_atomic get failed(ret=%d)\n", ret));
+			DBGPRINT(RT_DEBUG_ERROR, 
+					("reg_atomic get failed(ret=%d)\n", 
+					ret));
 			return STATUS_UNSUCCESSFUL;
 		}
 	}
 #endif /* RTMP_MAC_USB */
 
 	ret = STATUS_UNSUCCESSFUL;
-	do
-	{
+	do {
 		RTMP_IO_READ32(pAd, RF_CSR_CFG, &rfcsr.word);
 
 		if (!rfcsr.non_bank.RF_CSR_KICK)
 			break;
 		i++;
-	}
-	while ((i < MAX_BUSY_COUNT) && (!RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST)));
+	} while ((i < MAX_BUSY_COUNT) && (!RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST)));
 
-	if ((i == MAX_BUSY_COUNT) || (RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST)))
-	{
-		DBGPRINT(RT_DEBUG_ERROR, ("%s():RF Write failed(RetryCnt=%d, DevNotExistFlag=%d)\n",
-						__FUNCTION__, i, RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST)));
+	if ((i == MAX_BUSY_COUNT) || 
+		(RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST))) {
+		DBGPRINT(RT_DEBUG_ERROR, 
+				("%s():RF Write failed(RetryCnt=%d, DevNotExistFlag=%d)\n",
+				__FUNCTION__, i, RTMP_TEST_FLAG(pAd, 
+				fRTMP_ADAPTER_NIC_NOT_EXIST)));
 		goto done;
 	}
 
@@ -152,8 +131,8 @@ NDIS_STATUS RT30xxWriteRFRegister(
 	rfcsr.non_bank.RF_CSR_KICK = 1;
 	rfcsr.non_bank.TESTCSR_RFACC_REGNUM = regID;
 
-	if ((pAd->chipCap.RfReg17WtMethod == RF_REG_WT_METHOD_STEP_ON) && (regID == RF_R17))
-	{
+	if ((pAd->chipCap.RfReg17WtMethod == RF_REG_WT_METHOD_STEP_ON) && 
+		(regID == RF_R17)) {
 		UCHAR IdRf;
 		UCHAR RfValue;
 		BOOLEAN beAdd;
@@ -161,22 +140,20 @@ NDIS_STATUS RT30xxWriteRFRegister(
 		RT30xxReadRFRegister(pAd, RF_R17, &RfValue);
 		beAdd =  (RfValue < value) ? TRUE : FALSE;
 		IdRf = RfValue;
-		while(IdRf != value)
-		{
+		while (IdRf != value) {
 			if (beAdd)
 				IdRf++;
 			else
 				IdRf--;
-			
-				rfcsr.non_bank.RF_CSR_DATA = IdRf;
-				RTMP_IO_WRITE32(pAd, RF_CSR_CFG, rfcsr.word);
-				RtmpOsMsDelay(1);
+
+			rfcsr.non_bank.RF_CSR_DATA = IdRf;
+			RTMP_IO_WRITE32(pAd, RF_CSR_CFG, rfcsr.word);
+			RtmpOsMsDelay(1);
 		}
 	}
 
 	rfcsr.non_bank.RF_CSR_DATA = value;
 	RTMP_IO_WRITE32(pAd, RF_CSR_CFG, rfcsr.word);
-
 	ret = NDIS_STATUS_SUCCESS;
 
 done:
@@ -192,29 +169,16 @@ done:
 
 /*
 	========================================================================
-	
+
 	Routine Description: Read RF register through MAC
 
-	Arguments:
-
-	Return Value:
-
-	IRQL = 
-	
-	Note:
-	
 	========================================================================
 */
-NDIS_STATUS RT30xxReadRFRegister(
-	IN	PRTMP_ADAPTER	pAd,
-	IN	UCHAR			regID,
-	IN	PUCHAR			pValue)
+NDIS_STATUS RT30xxReadRFRegister(PRTMP_ADAPTER pAd, UCHAR regID, PUCHAR pValue)
 {
 	RF_CSR_CFG_STRUC rfcsr = { { 0 } };
-	UINT i=0, k=0;
-	NDIS_STATUS	 ret = STATUS_UNSUCCESSFUL;
-
-
+	UINT i = 0, k = 0;
+	NDIS_STATUS ret = STATUS_UNSUCCESSFUL;
 
 	ASSERT((regID <= pAd->chipCap.MaxNumOfRfId));
 
@@ -222,104 +186,103 @@ NDIS_STATUS RT30xxReadRFRegister(
 	if (IS_USB_INF(pAd)) {
 		RTMP_SEM_EVENT_WAIT(&pAd->reg_atomic, i);
 		if (i != 0) {
-			DBGPRINT(RT_DEBUG_ERROR, ("reg_atomic get failed(ret=%d)\n", i));
+			DBGPRINT(RT_DEBUG_ERROR, 
+					("reg_atomic get failed(ret=%d)\n", i));
 			return STATUS_UNSUCCESSFUL;
 		}
 	}
 #endif /* RTMP_MAC_USB */
 
-	for (i=0; i<MAX_BUSY_COUNT; i++)
-	{
-		if(RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST))
+	for (i = 0; i < MAX_BUSY_COUNT; i++) {
+		if (RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST))
 			goto done;
-			
+
 		RTMP_IO_READ32(pAd, RF_CSR_CFG, &rfcsr.word);
 
 		if (rfcsr.non_bank.RF_CSR_KICK == BUSY)
 				continue;
-		
+
 		rfcsr.word = 0;
 		rfcsr.non_bank.RF_CSR_WR = 0;
 		rfcsr.non_bank.RF_CSR_KICK = 1;
 		rfcsr.non_bank.TESTCSR_RFACC_REGNUM = regID;
 		RTMP_IO_WRITE32(pAd, RF_CSR_CFG, rfcsr.word);
-		
-		for (k=0; k<MAX_BUSY_COUNT; k++)
-		{
-			if(RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST))
+
+		for (k = 0; k < MAX_BUSY_COUNT; k++) {
+			if (RTMP_TEST_FLAG(pAd, fRTMP_ADAPTER_NIC_NOT_EXIST))
 				goto done;
-				
+
 			RTMP_IO_READ32(pAd, RF_CSR_CFG, &rfcsr.word);
 
 			if (rfcsr.non_bank.RF_CSR_KICK == IDLE)
 				break;
 		}
-		
+
 		if ((rfcsr.non_bank.RF_CSR_KICK == IDLE) &&
-			(rfcsr.non_bank.TESTCSR_RFACC_REGNUM == regID))
-		{
+			(rfcsr.non_bank.TESTCSR_RFACC_REGNUM == regID)) {
 			*pValue = (UCHAR)(rfcsr.non_bank.RF_CSR_DATA);
 			break;
 		}
 	}
 
-	if (rfcsr.non_bank.RF_CSR_KICK == BUSY)
-	{
+	if (rfcsr.non_bank.RF_CSR_KICK == BUSY) {
 		DBGPRINT_ERR(("%s(): RF read R%d=0x%X fail, i[%d], k[%d]\n",
-						__FUNCTION__, regID, rfcsr.word,i,k));
+				__FUNCTION__, regID, rfcsr.word,i,k));
 		goto done;
 	}
 	ret = STATUS_SUCCESS;
-	
+
 done:
 #ifdef RTMP_MAC_USB
 	if (IS_USB_INF(pAd)) {
 		RTMP_SEM_EVENT_UP(&pAd->reg_atomic);
 	}
 #endif /* RTMP_MAC_USB */
-	
+
 	return ret;
 }
 
 
 
 /*
-    ========================================================================
-    Routine Description:
-        Adjust frequency offset when do channel switching or frequency calabration.
-        
-    Arguments:
-        pAd         		- Adapter pointer
-        pRefFreqOffset	in: referenced Frequency offset   out: adjusted frequency offset
-        
-    Return Value:
-        None
-        
-    ========================================================================
+	========================================================================
+	Routine Description:
+		Adjust frequency offset when do channel switching or frequency calabration.
+
+	Arguments:
+		pAd		- Adapter pointer
+		pRefFreqOffset	in: referenced Frequency offset   
+				out: adjusted frequency offset
+
+	========================================================================
 */
 BOOLEAN RTMPAdjustFrequencyOffset(RTMP_ADAPTER *pAd,UCHAR *pRefFreqOffset)
 {
 	BOOLEAN RetVal = TRUE;
-	UCHAR RFValue = 0; 
-	UCHAR PreRFValue = 0; 
+	UCHAR RFValue;
+	UCHAR PreRFValue;
 	UCHAR FreqOffset = 0;
 	UCHAR HighCurrentBit = 0;
-	
+
 	RTMP_ReadRF(pAd, RF_R17, &FreqOffset, &HighCurrentBit, 0x7F);
 	PreRFValue =  HighCurrentBit | FreqOffset;
 	FreqOffset = min((*pRefFreqOffset & 0x7F), 0x5F);
 	RFValue = HighCurrentBit | FreqOffset;
-	if (PreRFValue != RFValue)
-	{
+
+	if (PreRFValue != RFValue) {
 #ifdef RTMP_MAC_USB
-		RetVal = AsicSendCommandToMcu(pAd, 0x74, 0xff, FreqOffset, PreRFValue, FALSE);
+		RetVal = AsicSendCommandToMcu(pAd, 0x74, 0xff, FreqOffset, 
+				PreRFValue, FALSE);
 #else
-		RetVal = (RT30xxWriteRFRegister(pAd, RF_R17, RFValue) == STATUS_SUCCESS ? TRUE:FALSE);
+		RetVal = (RT30xxWriteRFRegister(pAd, RF_R17, RFValue) == 
+				STATUS_SUCCESS ? TRUE:FALSE);
 #endif /* !RTMP_MAC_USB */
 	}
 
 	if (RetVal == FALSE)
-		DBGPRINT(RT_DEBUG_TRACE, ("%s(): Error in tuning frequency offset !!\n", __FUNCTION__));
+		DBGPRINT(RT_DEBUG_TRACE, 
+				("%s(): Error in tuning frequency offset !!\n", 
+				__FUNCTION__));
 	else
 		*pRefFreqOffset = FreqOffset;
 
