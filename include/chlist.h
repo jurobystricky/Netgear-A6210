4cfa7f0d6eba16a83d674d6c2a072ded83e81b23
Juro Bystricky <jurobystricky@hotmail.com>
2016-02-11 06:07:50 -0800

    chlist.h: Cleanup
    Removed some unused prototypes.
    Use C types for VOID and INT.
    
    Signed-off-by: Juro Bystricky <jurobystricky@hotmail.com>

--- include/chlist.h
+++ include/chlist.h
@@ -17,12 +17,6 @@
 
 	Module Name:
 	chlist.c
-
-	Abstract:
-
-	Revision History:
-	Who         When          What
-	--------    ----------    ----------------------------------------------
 */
 
 #ifndef __CHLIST_H__
@@ -43,6 +37,7 @@ typedef struct _COUNTRY_REGION_CH_DESC {
 }COUNTRY_REGION_CH_DESC, *PCOUNTRY_REGION_CH_DESC;
 
 #ifdef EXT_BUILD_CHANNEL_LIST
+
 #define ODOR	0
 #define IDOR	1
 #define BOTH	2
@@ -50,14 +45,14 @@ typedef struct _COUNTRY_REGION_CH_DESC {
 typedef struct _CH_DESP {
 	UCHAR FirstChannel;
 	UCHAR NumOfCh;
-	CHAR MaxTxPwr;			/* dBm */
-	UCHAR Geography;			/* 0:out door, 1:in door, 2:both */
-	BOOLEAN DfsReq;			/* Dfs require, 0: No, 1: yes. */
+	CHAR MaxTxPwr;		/* dBm */
+	UCHAR Geography;	/* 0:out door, 1:in door, 2:both */
+	BOOLEAN DfsReq;		/* Dfs require, 0: No, 1: yes. */
 } CH_DESP, *PCH_DESP;
 
 typedef struct _CH_REGION {
 	UCHAR CountReg[3];
-	UCHAR DfsType;			/* 0: CE, 1: FCC, 2: JAP, 3:JAP_W53, JAP_W56 */
+	UCHAR DfsType;		/* 0: CE, 1: FCC, 2: JAP, 3:JAP_W53, JAP_W56 */
 	CH_DESP *pChDesp;
 	BOOLEAN edcca_on;
 } CH_REGION, *PCH_REGION;
@@ -68,10 +63,10 @@ extern CH_REGION ChRegion[];
 #ifdef ED_MONITOR
 typedef struct _COUNTRY_PROP {
 	UCHAR CountReg[3];
-	UCHAR DfsType;			/* 0: CE, 1: FCC, 2: JAP, 3:JAP_W53, JAP_W56 */
+	UCHAR DfsType;		/* 0: CE, 1: FCC, 2: JAP, 3:JAP_W53, JAP_W56 */
 	BOOLEAN edcca_on;
 } COUNTRY_PROP, *PCOUNTRY_PROP;
-#endif /* ED_MONITOR */
+#endif
 
 #ifdef SINGLE_SKU_V2
 // TODO: shiang-usw, integrate these data structures to a single one!
@@ -97,8 +92,8 @@ typedef struct _CH_POWER_{
 #endif /* SINGLE_SKU_V2 */
 
 typedef struct _CH_FREQ_MAP_{
-	UINT16		channel;
-	UINT16		freqKHz;
+	UINT16 channel;
+	UINT16 freqKHz;
 }CH_FREQ_MAP;
 
 extern CH_FREQ_MAP CH_HZ_ID_MAP[];
@@ -107,62 +102,28 @@ extern int CH_HZ_ID_MAP_NUM;
 #define MAP_CHANNEL_ID_TO_KHZ(_ch, _khz)                 \
 		RTMP_MapChannelID2KHZ(_ch, (UINT32 *)&(_khz))
 #define MAP_KHZ_TO_CHANNEL_ID(_khz, _ch)                 \
-		RTMP_MapKHZ2ChannelID(_khz, (INT *)&(_ch))
+		RTMP_MapKHZ2ChannelID(_khz, (int *)&(_ch))
 
 /* Check if it is Japan W53(ch52,56,60,64) channel. */
 #define JapanChannelCheck(_ch)  ((_ch == 52) || (_ch == 56) || (_ch == 60) || (_ch == 64))
 
-#ifdef ED_MONITOR
-BOOLEAN GetEDCCASupport(
-	IN PRTMP_ADAPTER pAd);
+#ifdef EXT_BUILD_CHANNEL_LIST
+void BuildChannelListEx(PRTMP_ADAPTER pAd);
 #endif
 
-#ifdef EXT_BUILD_CHANNEL_LIST
-VOID BuildChannelListEx(
-	IN PRTMP_ADAPTER pAd);
-
-VOID BuildBeaconChList(
-	IN PRTMP_ADAPTER pAd,
-	OUT PUCHAR pBuf,
-	OUT	PULONG pBufLen);
-#endif /* EXT_BUILD_CHANNEL_LIST */
-
 #ifdef DOT11_N_SUPPORT
-VOID N_ChannelCheck(RTMP_ADAPTER *pAd);
+void N_ChannelCheck(RTMP_ADAPTER *pAd);
 UCHAR N_SetCenCh(RTMP_ADAPTER *pAd, UCHAR channel);
 BOOLEAN N_ChannelGroupCheck(RTMP_ADAPTER *pAd, UCHAR channel);
-#ifdef DOT11_VHT_AC
-INT get_vht_neighbor_index(UCHAR channel);
-BOOLEAN AC_ChannelGroupCheck(RTMP_ADAPTER *pAd, UCHAR channel);
-#endif /* DOT11_VHT_AC */
-#endif /* DOT11_N_SUPPORT */
+#endif
 
-UINT8 GetCountryMaxTxPwr(
-	IN PRTMP_ADAPTER pAd,
-	IN UINT8 channel);
+UINT8 GetCountryMaxTxPwr(PRTMP_ADAPTER pAd, UINT8 channel);
+void RTMP_MapChannelID2KHZ(UCHAR Ch, UINT32 *pFreq);
+void RTMP_MapKHZ2ChannelID(ULONG Freq, int *pCh);
+UCHAR GetChannel_5GHZ(PCH_DESC pChDesc, UCHAR index);
+UCHAR GetChannel_2GHZ(PCH_DESC pChDesc, UCHAR index);
+UCHAR GetChannelFlag(PCH_DESC pChDesc, UCHAR index);
+UINT16 TotalChNum(PCH_DESC pChDesc);
 
-VOID RTMP_MapChannelID2KHZ(
-	IN UCHAR Ch,
-	OUT UINT32 *pFreq);
-
-VOID RTMP_MapKHZ2ChannelID(
-	IN ULONG Freq,
-	OUT INT *pCh);
-
-UCHAR GetChannel_5GHZ(
-	IN PCH_DESC pChDesc, 
-	IN UCHAR index);
-
-UCHAR GetChannel_2GHZ(
-	IN PCH_DESC pChDesc, 
-	IN UCHAR index);
-
-UCHAR GetChannelFlag(
-	IN PCH_DESC pChDesc, 
-	IN UCHAR index);
-
-UINT16 TotalChNum(
-	IN PCH_DESC pChDesc);
-	
 #endif /* __CHLIST_H__ */
 
